abe4d830b92f5affef9951a4f7d6cd7e7044b356
Edson Alcala <edson_alcala@msn.com>
2021-03-03 02:57:15 +0000

    general info tests

--- src/hooks/useGeneralInfo.test.tsx
+++ src/hooks/useGeneralInfo.test.tsx
@@ -1,49 +1,52 @@
-import { act, renderHook } from "@testing-library/react-hooks"
+import { renderHook } from "@testing-library/react-hooks"
 import { ethers } from "ethers"
 import React from "react"
-import { EthereumAddress } from "../types"
-
-import {
-  UMASnapshotContainer,
-  getInjectedProvider,
-  PROVIDER_URL,
-  startUMASnapshotContainerOrSkip,
-  stopUMASnapshotContainerOrSkip,
-} from "../utils"
 
 import { ReactWeb3Provider } from "./useWeb3Provider"
 import { useGeneralInfo } from "./useGeneralInfo"
-import { deploySampleEMP } from "./utils"
 import { EMPProvider } from "./useEMPProvider"
-import { getUMAInterfaces, UMARegistryProvider } from "./useUMARegistry"
+import { UMARegistryProvider } from "./useUMARegistry"
+import { deployEMP, Ganache, getUMAInterfaces } from "../utils"
+import { buildFakeEMP } from "./faker"
 
 describe("useGeneralInfo tests", () => {
-  let umaSnapshotContainer: UMASnapshotContainer | undefined
+
+  let ganacheInstance: Ganache;
   let injectedProvider: ethers.providers.Web3Provider
-  let empAddress: EthereumAddress
-  let empInstance: ethers.Contract
+  let instance: ethers.Contract
 
   beforeAll(async () => {
-    umaSnapshotContainer = await startUMASnapshotContainerOrSkip()
-    injectedProvider = new ethers.providers.Web3Provider(getInjectedProvider(PROVIDER_URL))
-    const signer = injectedProvider.getSigner()
+    ganacheInstance = new Ganache({
+      port: 8549,
+      gasLimit: 10000000,
+    });
+    await ganacheInstance.start();
 
+    const ganacheProvider = ganacheInstance.server.provider;
+    injectedProvider = new ethers.providers.Web3Provider(ganacheProvider);
+
+    const network = await injectedProvider.getNetwork()
+    const signer = injectedProvider.getSigner();
+
+    const sampleEMP = buildFakeEMP()
+    const { expiringMultiPartyAddress } = await deployEMP(sampleEMP, network, signer)
     const allInterfaces = getUMAInterfaces()
-
-    // create sample EMP
-    empAddress = await deploySampleEMP(signer)
-    empInstance = new ethers.Contract(
-      empAddress,
-      allInterfaces.get("ExpiringMultiParty") as ethers.utils.Interface,
-      signer
-    )
+    const empInterface = allInterfaces.get('ExpiringMultiParty')
+    if (!empInterface) {
+      throw new Error("Couldn't find the EMP interface")
+    }
+    instance = new ethers.Contract(expiringMultiPartyAddress, empInterface, signer)
   })
+
+  afterAll(async () => {
+    await ganacheInstance.stop();
+  });
 
   const render = () => {
     const wrapper = ({ children }: any) => (
       <UMARegistryProvider>
         <ReactWeb3Provider injectedProvider={injectedProvider}>
-          <EMPProvider empInstance={empInstance}>{children}</EMPProvider>
+          <EMPProvider empInstance={instance}>{children}</EMPProvider>
         </ReactWeb3Provider>
       </UMARegistryProvider>
     )
@@ -57,13 +60,12 @@ describe("useGeneralInfo tests", () => {
     await waitForNextUpdate()
     await waitForNextUpdate()
     await waitForNextUpdate()
+    await waitForNextUpdate()
+    await waitForNextUpdate()
+    await waitForNextUpdate()
 
     expect(result.current).toBeDefined()
 
     console.log("Result", result.current)
-  })
-
-  afterAll(async () => {
-    await stopUMASnapshotContainerOrSkip(umaSnapshotContainer)
   })
 })
