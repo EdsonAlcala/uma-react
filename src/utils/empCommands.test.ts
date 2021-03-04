3d5a8ce80da35f04e1e2bca458d54facc906233d
Edson Alcala <edson_alcala@msn.com>
2021-03-04 23:40:45 +0000

    added create method

--- src/utils/empCommands.test.ts
+++ src/utils/empCommands.test.ts
@@ -1,4 +1,4 @@
-import { ethers } from 'ethers'
+import { BigNumber, ethers } from 'ethers'
 
 import { buildFakeEMP } from '../fakers'
 import { EMPData, EthereumAddress } from '../types'
@@ -8,6 +8,8 @@ import { create, deposit, withdraw, redeem } from './empCommands'
 import { getUMAInterfaces } from './umaInterfaces'
 import { deployEMP } from './deployEMP'
 import { Ganache } from './ganache'
+import { toWeiSafe } from './conversions'
+import { getAllEMPData } from '../hooks'
 
 describe("empCommands", () => {
     let empAddress: EthereumAddress
@@ -17,6 +19,10 @@ describe("empCommands", () => {
     let empInstance: ethers.Contract
     let empData: EMPData
     let injectedProvider: ethers.providers.Web3Provider
+    let collateralInstance: ethers.Contract
+    let syntheticInstance: ethers.Contract
+    let collateralDecimals: number
+    let tokenDecimals: number
 
     beforeAll(async () => {
         ganacheInstance = new Ganache({
@@ -41,14 +47,40 @@ describe("empCommands", () => {
             allInterfaces.get('ExpiringMultiParty') as ethers.utils.Interface,
             signer,
         )
+        empData = await getAllEMPData(empInstance)
+        collateralInstance = new ethers.Contract(empData.collateralCurrency, allInterfaces.get('ERC20'), signer)
+        syntheticInstance = new ethers.Contract(empData.tokenCurrency, allInterfaces.get('ERC20'), signer)
+        collateralDecimals = await getTokenDecimals(collateralInstance)
+        tokenDecimals = await getTokenDecimals(syntheticInstance)
+
+        console.log("collateralDecimals", collateralDecimals)
+        console.log("tokenDecimals", tokenDecimals)
     })
 
     afterAll(async () => {
         await ganacheInstance.stop()
     })
 
-    test('create', async () => {
+    const getTokenDecimals = async (contractInstance: ethers.Contract): Promise<number> => {
+        return await contractInstance.decimals()
+    }
 
+    const setMaxAllowance = async (contractInstance: ethers.Contract, empAddressParameter: EthereumAddress): Promise<void> => {
+        const receipt = await contractInstance.approve(empAddressParameter, ethers.constants.MaxUint256)
+        await receipt.wait()
+        return receipt
+    }
+
+    test('create', async () => {
+        const numberOfCollateral = 1000;
+        const numberOfTokens = 100;
+
+        const maxAllowanceReceipt = await setMaxAllowance(collateralInstance, empAddress)
+        expect(maxAllowanceReceipt).toBeDefined()
+
+        const result = await create(empInstance, toWeiSafe(numberOfCollateral.toString(), collateralDecimals), toWeiSafe(numberOfTokens.toString(), tokenDecimals))
+
+        expect(result).toBeDefined()
     })
 
     test('deposit', async () => {
--- src/utils/empCommands.ts
+++ src/utils/empCommands.ts
@@ -3,19 +3,23 @@ import { BigNumber, ethers } from "ethers";
 export const create = async (instance: ethers.Contract, collateral: BigNumber, tokens: BigNumber) => {
     const receipt = await instance.create([collateral], [tokens])
     await receipt.wait();
+    return receipt
 }
 
 export const deposit = async (instance: ethers.Contract, collateralToDeposit: BigNumber) => {
     const receipt = await instance.deposit([collateralToDeposit])
     await receipt.wait();
+    return receipt
 }
 
 export const redeem = async (instance: ethers.Contract, tokensToRedeem: BigNumber) => {
     const receipt = await instance.redeem([tokensToRedeem])
     await receipt.wait();
+    return receipt
 }
 
 export const withdraw = async (instance: ethers.Contract, collateralToWithdraw: BigNumber) => {
     const receipt = await instance.withdraw([collateralToWithdraw])
     await receipt.wait();
+    return receipt
 }
