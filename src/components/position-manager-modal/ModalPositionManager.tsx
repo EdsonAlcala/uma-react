19463405740410635576542be4da1eb7e0684e36
Edson Alcala <edson_alcala@msn.com>
2021-07-02 21:15:53 -0500

    fixed the issue with a generator class

--- src/components/position-manager-modal/ModalPositionManager.tsx
+++ src/components/position-manager-modal/ModalPositionManager.tsx
@@ -3,6 +3,7 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogContent'
 
 import { PositionManager } from '../position-manager/Main'
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles'
 
 export interface ModalPositionManagerProps {
     open: boolean
@@ -10,12 +11,19 @@ export interface ModalPositionManagerProps {
     onlyCreate?: boolean
 }
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'cx',
+});
+
+
 export const ModalPositionManager: React.FC<ModalPositionManagerProps> = ({ open, handleClose, onlyCreate }) => {
     return (
         <Dialog fullWidth={true} maxWidth="md" open={open} onClose={handleClose}>
-            <DialogContent style={{ backgroundColor: 'white', display: 'flex', height: 320, padding: 0, paddingTop: '0', alignItems: 'center' }}>
-                <PositionManager onlyCreate={onlyCreate} />
-            </DialogContent>
+            <StylesProvider generateClassName={generateClassName}>
+                <DialogContent style={{ backgroundColor: 'white', display: 'flex', height: 320, padding: 0, paddingTop: '0', alignItems: 'center' }}>
+                    <PositionManager onlyCreate={onlyCreate} />
+                </DialogContent>
+            </StylesProvider>
         </Dialog>
     )
 }
