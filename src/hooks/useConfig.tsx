a6fb50967e669aa1461b9d0ebcb58eb31dd9fc31
Edson Alcala <edson_alcala@msn.com>
2021-07-04 20:56:18 -0500

    fix export

--- src/hooks/useConfig.tsx
+++ src/hooks/useConfig.tsx
@@ -14,7 +14,7 @@ const ConfigContext = React.createContext<IConfigProvider>({
     umaAPIBaseUrl: '',
 })
 
-interface Props extends IConfigProvider {}
+interface Props extends IConfigProvider { }
 
 export const ConfigProvider: React.FC<PropsWithChildren<Props>> = ({ children, ...props }) => {
     const [onboardAPIKey, setOnboardAPIKey] = useState('')
