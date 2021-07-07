bbece2159eae9b0e2e050074107769c658e2bfae
Edson Alcala <edson_alcala@msn.com>
2021-07-07 09:32:55 -0500

    ignore lint

--- src/components/common/Links.tsx
+++ src/components/common/Links.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { PropsWithChildren } from 'react'
 
+// eslint-disable-next-line
 export const MaxLink: React.FC<PropsWithChildren<{}>> = ({ children }) => {
     return <div style={{ textDecoration: 'underline' }}>{children}</div>
 }
 
+// eslint-disable-next-line
 export const MinLink: React.FC<PropsWithChildren<{}>> = ({ children }) => {
     return <div style={{ textDecoration: 'underline' }}>{children}</div>
 }
