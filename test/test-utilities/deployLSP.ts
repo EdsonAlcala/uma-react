80ae74ebb3b9504154365f02e9bc0478f93834f7
Edson Alcala <edson_alcala@msn.com>
2021-07-07 19:22:52 -0500

    lint and prettier

--- test/test-utilities/deployLSP.ts
+++ test/test-utilities/deployLSP.ts
@@ -19,17 +19,17 @@ export const deployLSP = async (values: LSPParameters, network: ethers.providers
         expirationTimestamp: expirationTimestamp.toString(),
         collateralPerPair: toWei(`${collateralPerPair}`),
         priceIdentifier: '0x554d415553440000000000000000000000000000000000000000000000000000',
-        longSynthName: "UMA 25 USD Call [July 2021]",
-        longSynthSymbol: "UMAc25-0721-L",
-        shortSynthName: "UMA 25 USD Covered Call [July 2021]",
-        shortSynthSymbol: "UMAc25-0721-S",
+        longSynthName: 'UMA 25 USD Call [July 2021]',
+        longSynthSymbol: 'UMAc25-0721-L',
+        shortSynthName: 'UMA 25 USD Covered Call [July 2021]',
+        shortSynthSymbol: 'UMAc25-0721-S',
         collateralToken: collateralToken,
         financialProductLibrary: financialProductLibraryAddress,
-        customAncillaryData: "0",
+        customAncillaryData: '0',
         prepaidProposerReward: 0,
-        pairName: "test",
-        optimisticOracleProposerBond: "0",
-        optimisticOracleLivenessTime: "0"
+        pairName: 'test',
+        optimisticOracleProposerBond: '0',
+        optimisticOracleLivenessTime: '0',
     }
 
     const umaABIs = getUMAAbis()
@@ -44,18 +44,18 @@ export const deployLSP = async (values: LSPParameters, network: ethers.providers
     if (!lspCreatorAddress) {
         throw new Error('Invalid LongShortPairCreator Address')
     }
-    console.log("lspCreatorAddress", lspCreatorAddress)
+    console.log('lspCreatorAddress', lspCreatorAddress)
     const lspCreator = new ethers.Contract(lspCreatorAddress, lspCreatorInterface, signer)
-    console.log("Params", params)
+    console.log('Params', params)
     const lspAddress = await lspCreator.callStatic.createLongShortPair(params, {
-        value: 0
+        value: 0,
     })
 
     console.log('lspAddress', lspAddress)
 
-    // const txn = await lspCreator.createLongShortPair(params)
+    const txn = await lspCreator.createLongShortPair(params)
 
-    // const receipt: ContractReceipt = await txn.wait()
+    const receipt: ContractReceipt = await txn.wait()
 
-    return { receipt: "asd" as any, lspAddress }
+    return { receipt, lspAddress }
 }
